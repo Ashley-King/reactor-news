28de72038e7684eaa8ce109014cf2308323bc9bb
Ashley King <ashleyot@gmail.com>
2019-05-02 13:48:04 -0400

    original commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,1 @@
 
-import React, {Component} from 'react'
-import './App.css'
-
-class App extends Component{
-
-  render() {
-    let helloWorld = 'Welcome to React!'
-    return(
-      
-      <div className="App">
-        <h2>{helloWorld}</h2>
-      </div>
-    )
-  }
-}
-
-export default App;
