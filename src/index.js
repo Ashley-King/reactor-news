18396faed8e2db677b057b59c5470ce7562788bd
Ashley King <ashleyot@gmail.com>
2019-05-02 16:43:47 -0400

    rewriting app; start up on page 52

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-im
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+
+ReactDOM.render(
+ <App />,
+ document.getElementById('root')
+);
+
+if(module.hot){
+ module.hot.accept();
+}
